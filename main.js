78b19a73fdcd5c08931e939fc595c1057ab7e7e5
Andrew Ilyin <a13andrew@gmail.com>
2020-03-19 15:19:42 +0200

    added preview

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-
 var uploadButton = document.getElementById("upload-button");
-var uploadInput = document.getElementById("upload-button");
+var uploadInput = document.getElementById("upload-input");
 
 
 uploadButton.onclick = (e) => {
@@ -9,4 +8,19 @@ uploadButton.onclick = (e) => {
 
 uploadInput.oninput = (e) => {
     console.log("[uploadInput] " + e.type);
+}
+
+
+uploadInput.onchange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function (e) {
+            document.getElementById("preview-img").src = e.target.result;
+        };
+    }
+
+    console.log(e);
 }
