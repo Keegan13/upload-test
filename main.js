ffc61a94f4f1e2e36ee422dd815795bad7b92b21
Andrew Ilyin <a13andrew@gmail.com>
2020-03-19 15:33:03 +0200

    changed to object url

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ var uploadInput = document.getElementById("upload-input");
 uploadInput.onchange = (e) => {
     const file = e.target.files[0];
     if (file) {
-        var reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function (e) {
-            document.getElementById("preview-img").src = e.target.result;
-        };
+        document.getElementById("preview-img").src = URL.createObjectURL(file);
     }
 }
