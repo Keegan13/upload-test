dd01ce68e37da57ab74f615068eed88f7da0fb6f
Andrew Ilyin <a13andrew@gmail.com>
2020-03-19 15:24:46 +0200

    fix

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-var uploadButton = document.getElementById("upload-button");
 var uploadInput = document.getElementById("upload-input");
-
-
-uploadButton.onclick = (e) => {
-    console.log("[uploadButton] " + e.type);
-}
-
-uploadInput.oninput = (e) => {
-    console.log("[uploadInput] " + e.type);
-}
-
 
 uploadInput.onchange = (e) => {
     const file = e.target.files[0];
-
     if (file) {
         var reader = new FileReader();
         reader.readAsDataURL(file);
@@ -21,6 +9,4 @@ uploadInput.onchange = (e) => {
             document.getElementById("preview-img").src = e.target.result;
         };
     }
-
-    console.log(e);
 }
